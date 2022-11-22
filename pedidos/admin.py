1a44def1aacae7136febb7831bda0b2b6bc10cef
German Olate Quezada <golate@megalabs.cl>
2022-11-22 15:59:21 -0300

    first commit

---
+++ pedidos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from Pedidos.models import Dim_cliente
+
+
+admin.site.register(Dim_cliente)
---
+++ pedidos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+from django.template.loader import get_template
+
+
+# Create your views here.
+
+def Pedidos(request):
+    
+    fecha_actual=datetime.datetime.now()
+    return render(request,"pedidos.html", {"dameFecha":fecha_actual})   
