1b03e8a14066924142d3beb24a66fcb8af016cce
germanolatequezada <118131923+germanolatequezada@users.noreply.github.com>
2022-11-26 16:48:24 -0300

    admin

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from Pedidos.models import Dim_cliente
+from Pedidos.models import Dim_mesa
 
 
 admin.site.register(Dim_cliente)
+admin.site.register(Dim_mesa)
