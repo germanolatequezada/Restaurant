5c6945f7dc9cf11af8055cc5273a9cc5a3613ae3
germanolatequezada <118131923+germanolatequezada@users.noreply.github.com>
2022-11-26 21:19:07 -0300

    SQl

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-       # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
 
       #  'ENGINE': 'sql_server.pyodbc',
